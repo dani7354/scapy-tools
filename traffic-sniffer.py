6402d52649eec49f694385c7c7aaf5929d46fe08
Daniel <d@stuhrs.dk>
2023-04-07 17:50:27 +0200

    write packets to pcap file

--- traffic-sniffer.py
+++ traffic-sniffer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from scapy.all import sniff
+from scapy.all import sniff, wrpcap
 
 
 INTERFACE="en0"
 FILTER="tcp port 443 or tcp port 80"
-
+OUTPUT="capture.pcap"
 
 def show_packet(packet):
     # TODO: add filtering if needed!
@@ -12,7 +12,8 @@ def show_packet(packet):
 
 
 def main():
-     sniff(iface=INTERFACE, filter=FILTER, prn=show_packet, store=0)
+     packets = sniff(iface=INTERFACE, filter=FILTER, prn=show_packet)
+     wrpcap(OUTPUT, packets)
 
 
 if __name__ == "__main__":
