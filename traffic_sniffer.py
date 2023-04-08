bad1f632d3639a4dd87f792435215416ea74135f
Daniel <d@stuhrs.dk>
2023-04-08 14:44:03 +0200

    added arguments to traffic_sniffer

--- traffic_sniffer.py
+++ traffic_sniffer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 from scapy.all import sniff, wrpcap
 import time
 
-
-INTERFACE = "any"
-FILTER = ""  # BPF syntax
 
 packets = []
 
@@ -15,8 +12,10 @@ def get_default_pcap():
     return f"capture_{time_seconds}.pcap"
 
 
-def parse_arguments():
+def parse_arguments() -> Namespace:
     parser = ArgumentParser(description="Sniffs packets and saves them to pcap")
+    parser.add_argument("-i", "--interface", dest="interface", type=str, required=False, default="any")
+    parser.add_argument("-f", "--filter", dest="filter", type=str, required=False, default="")
     parser.add_argument("-o", "--output", dest="output", type=str, required=False, default=get_default_pcap())
 
     return parser.parse_args()
@@ -30,8 +29,8 @@ def save_packet(packet):
 def main():
     args = parse_arguments()
 
-    print(f"Sniffing packets on interface {INTERFACE} with filter {FILTER}...")
-    sniff(iface=INTERFACE, filter=FILTER, prn=save_packet, store=0)
+    print(f"Sniffing packets on interface {args.interface} with filter {args.filter}...")
+    sniff(iface=args.interface, filter=args.filter, prn=save_packet, store=0)
 
     print(f"Saving captured packets to {args.output}...")
     wrpcap(args.output, packets)
@@ -39,4 +38,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
