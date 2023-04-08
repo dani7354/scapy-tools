e67d78425539b71f5f27797bb611ce0ab6a73c5b
Daniel <d@stuhrs.dk>
2023-04-08 07:36:24 +0200

    added argument

--- traffic-sniffer.py
+++ traffic-sniffer.py
@@ -1,19 +1,40 @@
 #!/usr/bin/env python3
+from argparse import ArgumentParser
 from scapy.all import sniff, wrpcap
+import time
 
 
-INTERFACE="en0"
-FILTER="tcp port 443 or tcp port 80"
-OUTPUT="capture.pcap"
+INTERFACE = "lo0"
+FILTER = ""
 
-def show_packet(packet):
-    # TODO: add filtering if needed!
-    print(packet.show())
+packets = []
+
+
+def get_default_pcap():
+    time_seconds = int(time.time())
+    return f"capture_{time_seconds}.pcap"
+
+
+def parse_arguments():
+    parser = ArgumentParser(description="Sniffs packets and saves them to pcap")
+    parser.add_argument("-o", "--output", dest="output", type=str, required=False, default=get_default_pcap())
+
+    return parser.parse_args()
+
+
+def save_packet(packet):
+    packets.append(packet)
+    print(packet.summary())
 
 
 def main():
-     packets = sniff(iface=INTERFACE, filter=FILTER, prn=show_packet)
-     wrpcap(OUTPUT, packets)
+    args = parse_arguments()
+    
+    print(f"Sniffing packets on interface {INTERFACE} with filter {FILTER}...")
+    sniff(iface=INTERFACE, filter=FILTER, prn=save_packet, store=0)
+
+    print(f"Saving captured packets to {args.output}...")
+    wrpcap(args.output, packets)
 
 
 if __name__ == "__main__":
